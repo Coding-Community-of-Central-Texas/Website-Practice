19d537f1bca46c6c4f9846cc13c27ed196a5e1fa
Amber Feeley <amberfeeley@yahoo.com>
2025-09-09 20:57:05 -0500

    test6

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <HashRouter basename="/Website-Practice/">
       <App />
     </HashRouter>
   </StrictMode>,
