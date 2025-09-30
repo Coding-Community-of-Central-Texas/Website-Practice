79a5f8dd9821c694f4a6483b489fa9d6ea59090f
GarmadonMagic <142450744+GarmadonMagic@users.noreply.github.com>
2025-09-29 20:21:46 -0500

    Socials to Nav Bar (#27)
    * Added Discord, Notion, Presence, and Instagram socials to the NavBar. Made lots of display: flex changes to make it all work.
    
    * Notion Link Fix
    
    Old link added new notion page. This fixes it.
    
    * Fixed Header/Socials
    
    IDs replaced with classes, #navLinks replaced with .nav-links-container to avoid confusion, css for socials added
    
    * Removed Socials Class
    
    Socials class wasn't necessary (no valuable difference), so the class was removed.
    
    * Fixed Hover Issue
    
    Added a class for the page links to get the underline hover effect working again.

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import "./NavBar.css";
 import { NavLink } from "react-router-dom";
+import instagramLogo from "../../images/instagram-logo.png";
+import discordLogo from "../../images/discord-logo.png";
+import presenceLogo from "../../images/presence-logo.png";
+import notionLogo from "../../images/Notion-logo.png";
 
 export default function NavBar() {
   return (
     <nav className="nav-links">
-      <div id="navLinks">
+      <div className="nav-links-container">
         <ul>
-          <li>
+          <li className="page-link">
             <NavLink to="home">Home</NavLink>
           </li>
-          <li>
+          <li className="page-link">
             <NavLink to="members">Members</NavLink>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="https://www.instagram.com/oscc_ct/"><img src={instagramLogo} alt= "@oscc_ct"/></a>
+          </li>
+          <li>
+            <a href="https://discord.gg/NJ4U5jrBbX"><img src={discordLogo} alt="Coding Club"/></a>
+          </li>
+          <li>
+            <a href="https://tamuct.presence.io/organization/open-source-coding-community-of-central-texas"><img src={presenceLogo} alt="Presence"/></a>
+          </li>
+          <li>
+            <a href="https://www.notion.so/osccct/Open-Source-Coding-Community-of-Central-Texas-OSCCCT"><img src={notionLogo} alt="Notion"/></a>
           </li>
         </ul>
       </div>
